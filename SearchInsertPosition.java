deea27e44f94b6c3fba7523fa0b5709686a34520
Saksham Tiwari <contact@tsaksham.com>
2020-02-27 19:27:56 -0500

    Search Insert Position commit
    Search Insert Position commit

---
+++ SearchInsertPosition.java
@@ -0,0 +1,11 @@
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
