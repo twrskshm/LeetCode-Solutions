71c5b07e10020fa3f6131a79070e71e7a19a28e0
Saksham Tiwari <contact@tsaksham.com>
2020-03-04 03:54:02 -0500

    Remove Element commit
    Remove Element commit

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,12 +1,10 @@
 public class RemoveElement {
     public static int removeElement(int[] nums, int val) {
-        int swap_index = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != val) {
-                nums[swap_index++] = nums[i];
-            }
-        }
+        int i = 0;
 
-        return swap_index;
+        for(int element: nums)
+            if(element != val) nums[i++] = element;
+
+        return i;
     }
 }
