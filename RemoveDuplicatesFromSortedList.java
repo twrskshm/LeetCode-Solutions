0298e388c3e834832e678682ec25015f60f392d8
Saksham Tiwari <contact@tsaksham.com>
2020-03-04 03:41:57 -0500

    Remove Duplicates From Sorted List commit
    Remove Duplicates From Sorted List commit

---
+++ RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicatesFromSortedList {
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while(current != null && current.next != null) {
+            if(current.val == current.next.val) current.next = current.next.next;
+            else current = current.next;
+        }
+
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
