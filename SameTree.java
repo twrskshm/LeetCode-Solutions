83fb6cd876afd560522fca919c7ae31ca73fce3c
Saksham Tiwari <contact@tsaksham.com>
2020-03-04 19:42:30 -0500

    Same Tree commit
    Same Tree commit

---
+++ SameTree.java
@@ -0,0 +1,19 @@
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
