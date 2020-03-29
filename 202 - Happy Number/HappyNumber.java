b08cdbe902e2f4740bdcfcaf6fe20115cb1d6d3f
Saksham Tiwari <contact@tsaksham.com>
2020-03-29 03:19:29 -0400

    Happy Number commit
    Happy Number commit

---
+++ 202 - Happy Number/HappyNumber.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class HappyNumber {
+    public static boolean isHappy(int n) {
+        HashSet<Integer> seenBefore = new HashSet<>();
+
+        while (n != 1) {
+            if(seenBefore.contains(n))
+                return false;
+
+            int temporaryN = n, sum = 0;
+
+            while (n > 0) {
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+
+            seenBefore.add(temporaryN);
+            n = sum;
+        }
+
+        return true;
+    }
+}
