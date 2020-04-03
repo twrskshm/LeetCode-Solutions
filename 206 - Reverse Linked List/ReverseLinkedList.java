49c2fc660d78fd5adf10f266560eeac356519b55
Saksham Tiwari <contact@tsaksham.com>
2020-04-03 01:38:26 -0400

    Reverse Linked List update
    Reverse Linked List update

--- 206 - Reverse Linked List/ReverseLinkedList.java
+++ 206 - Reverse Linked List/ReverseLinkedList.java
@@ -2,6 +2,8 @@
 Reverse a singly linked list.
 */
 
+import com.company.LinkedList;
+
 public class ReverseLinkedList {
     public static ListNode reverseList(ListNode head) {
         ListNode result = null;
@@ -11,6 +13,28 @@ public class ReverseLinkedList {
             head.next = result;
             result = head;
             head = temporaryNode;
+        }
+
+        return result;
+    }
+
+    public static com.company.ListNode reverseList(com.company.ListNode head) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        while (head != null) {
+            linkedList.add(0, head.val);
+            head = head.next;
+        }
+
+        com.company.ListNode currentNode = null, result = null;
+        for (int value: linkedList) {
+            if (currentNode == null) {
+                currentNode = new com.company.ListNode(value);
+                result = currentNode;
+            } else {
+                currentNode.next = new com.company.ListNode(value);
+                currentNode = currentNode.next;
+            }
         }
 
         return result;
